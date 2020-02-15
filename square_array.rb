650079631e88ba460093060c8b76c4440802a013
Jen Miao <jenmiao@gmail.com>
2020-02-15 22:28:33 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # your code here
   numbers = [1,2,3]
-  newArr = array.each do |numbers]
-    puts numbers * 2
+  newArr = array.each do |numbers|
+    puts numbers*2
   newArr
 end
